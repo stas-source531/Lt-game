c00e5a7044bd8d18a53b688f5df292fa6957eef3
stas-source531 <frundzya@gmail.com>
2026-09-12 07:43:43 +0300

    обнова

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788783558|11113784';
+const CACHE_VERSION = '1789188210|15704732';
 /** @type {string} */
 const CACHE_PREFIX = 'Литовская игра-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
